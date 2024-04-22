3cdfdd23bb4d35346c1bee8ae8365731cb82b18b
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-04-22 19:21:57 +0300

    Add files via upload

---
+++ 5.unite uygulama 17/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5.unite_uygulama_17
+{
+    public partial class Form1 : Form
+    {
+       ErrorProvider ep=new ErrorProvider();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKAydet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumara_Validating(object sender, CancelEventArgs e)
+        {
+            if(int.TryParse(txtNumara.Text,out int sonuc))
+            {
+                ep.SetError(txtNumara ,"");
+            }
+            else
+            {
+                e.Cancel = true;
+                ep.SetError(txtNumara, "numara hatalı");
+            }
+        }
+
+        private void txtAdSoyad_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtAdSoyad.Text == "")
+            {
+                e.Cancel = true;
+                ep.SetError(txtNumara, "ad ve soyad giriniz");
+            }
+            else
+            {
+                ep.SetError(txtAdSoyad ,"");
+            }
+        }
+
+        private void txtDersNotu_Validating(object sender, CancelEventArgs e)
+        {
+            int dersnotu;
+            if (int.TryParse(txtDersNotu.Text, out dersnotu))
+            {
+                if(dersnotu < 0 || dersnotu > 100)
+                {
+                    e.Cancel = true;
+                    ep.SetError(txtNumara, " 0-100 arasında giriniz");
+                }
+                else
+                {
+                    ep.SetError(txtDersNotu, "");
+                }
+
+            }
+            else
+            {
+                e.Cancel = true;
+                ep.SetError(txtDersNotu, " sayısal deger giriniz");
+            }
+        }
+    }
+}
